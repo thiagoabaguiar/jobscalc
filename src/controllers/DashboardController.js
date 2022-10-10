59a66a6c8faba1c2288a30b96d6717bc9ca2699c
Thiago Aguiar <thiagoabaguiar@gmail.com>
2022-10-10 11:43:38 -0300

    Add files via upload

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,46 @@
+const Job = require("../model/Job")
+const Profile = require("../model/Profile")
+const JobUtils = require("../utils/JobUtils")
+
+module.exports = {
+
+    async index(req, res) {
+
+        let profile = await Profile.get()
+
+        if (profile != undefined) {
+
+            const allJobs = await Job.getAll()
+
+            let statusCount = {
+                progress: 0,
+                done: 0,
+                total: allJobs.length
+            }
+
+            let hoursJobInProgress = 0
+            const infoJobs = allJobs.map((job) => {
+                const remaining = JobUtils.remainingDays(job);
+                const status = remaining <= 0 ? "done" : "progress";
+                statusCount[status] += 1
+                hoursJobInProgress = (status === "progress") ? hoursJobInProgress + Number(job.daily_hours) : hoursJobInProgress
+
+                return {
+                    ...job,
+                    remaining,
+                    status,
+                    budget: job.budget,
+                }
+            })
+
+            const freeHours = profile.hours_per_day - hoursJobInProgress
+
+            res.render("index", { jobs: infoJobs, profile, statusCount, freeHours })
+        }
+        else {
+
+            res.redirect("/profile")
+
+        }
+    }
+}
